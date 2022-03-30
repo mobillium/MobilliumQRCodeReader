2af0c8ee6f432e0ab5f9760b3ace974555b9515f
mrtcelebi <mrttcelebi@gmail.com>
2022-03-30 12:07:21 +0300

    remove main.storyboard and configure app start

--- Example/MobilliumQRCodeReader/ViewController.swift
+++ Example/MobilliumQRCodeReader/ViewController.swift
@@ -12,6 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
     }
 }
