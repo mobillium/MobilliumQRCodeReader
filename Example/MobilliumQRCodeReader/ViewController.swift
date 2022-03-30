37bc7e3b2c99e16b710c0e598ef8f571d155fdd0
mrtcelebi <mrttcelebi@gmail.com>
2022-03-30 11:50:26 +0300

    update min deployment version

--- Example/MobilliumQRCodeReader/ViewController.swift
+++ Example/MobilliumQRCodeReader/ViewController.swift
@@ -14,11 +14,4 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
-
