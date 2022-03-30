3d6d323a1445144c803173be4138bd388936c9f3
mrtcelebi <mrttcelebi@gmail.com>
2022-03-30 11:17:31 +0300

    initial comment with cocoapod lib template

---
+++ Example/Pods/Target Support Files/MobilliumQRCodeReader/MobilliumQRCodeReader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MobilliumQRCodeReader : NSObject
+@end
+@implementation PodsDummy_MobilliumQRCodeReader
+@end
---
+++ Example/Pods/Target Support Files/Pods-MobilliumQRCodeReader_Example/Pods-MobilliumQRCodeReader_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MobilliumQRCodeReader_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MobilliumQRCodeReader_Example
+@end
